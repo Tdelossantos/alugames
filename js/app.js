4f9bed2eb3851715620197c171419e289716880b
Tabaré De Los Santos <m.tabaredelossantos@gmail.com>
2025-08-19 13:55:24 -0300

    definindo variaveis

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-print("olá mundo");
+function alterarStatus(id){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector(".dashboard__item__img");
+    let botao = gameClicado.querySelector(".dashboard__item__button");
+    let nomeJogo = gameClicado.querySelector(".dashboard__item__name");
+}
